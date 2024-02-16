cf0e9c3ef0533ae78872ac334acf35dc67a2c767
Oğuzhan Turan <oguzhan_turan_52@hotmail.com>
2024-02-16 14:50:17 +0300

    Add project files.

---
+++ KidKinder/Controllers/DefaultController.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using KidKinder.Entities;
+using KidKinder.Context;
+
+namespace KidKinder.Controllers
+{
+    public class DefaultController : Controller
+    {
+        KidKinderContext context = new KidKinderContext();
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult PartialHead()
+        {
+            return PartialView();
+        }
+
+        public ActionResult PartialNavBar()
+        {
+            return PartialView();
+        }
+
+        public ActionResult PartialFeature()
+        {
+            var values = context.Features.ToList();
+            return PartialView(values);
+        }
+
+        public ActionResult PartialService()
+        {
+            var values = context.Services.ToList();
+            return PartialView(values);
+        }
+
+        public ActionResult PartialAbout()
+        {
+            return PartialView();
+        }
+
+        public ActionResult PartialClassRooms()
+        {
+            var values = context.ClassRooms.ToList();
+            return PartialView(values);
+        }
+
+        public ActionResult PartialBookASeat()
+        {
+            return PartialView();
+        }
+
+        public ActionResult PartialTeacher()
+        {
+            return PartialView();
+        }
+
+        public ActionResult PartialTestimonial()
+        {
+            return PartialView();
+        }
+
+        public ActionResult PartialFooter()
+        {
+            return PartialView();
+        }
+
+        public ActionResult PartialScript()
+        {
+            return PartialView();
+        }
+
+
+
+    }
+}
---
+++ KidKinder/Models/SendMessageViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace KidKinder.Models
+{
+    public class SendMessageViewModel
+    {
+        [Required(ErrorMessage = "Lütfen ad bilgisii boş geçmeyin")]
+        [StringLength(30,ErrorMessage = "Lütfen en fazla 30 karakter veri girişi yapınız")]
+        public string NameSurname { get; set; }
+
+        [Required(ErrorMessage = "Lütfen email bilgisini boş geçmeyin")]
+        [StringLength(50, ErrorMessage = "Lütfen en fazla 50 karakter veri girişi yapınız")]
+        [EmailAddress(ErrorMessage = "Lütfen geçerli bir email adresi giriniz")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Lütfen konu bilgisini boş geçmeyin")]
+        [StringLength(50, ErrorMessage = "Lütfen en fazla 50 karakter veri girişi yapınız")]
+        [MinLength(5,ErrorMessage = "Lütfen en az 5 karakter veri girişi yapınız")]
+        public string Subject { get; set; }
+
+        [Required(ErrorMessage = "Lütfen mesaj bilgisini boş geçmeyin")]
+        [StringLength(1000, ErrorMessage = "Lütfen daha az karakter veri girişi yapınız")]
+        public string Message { get; set; }
+    }
+}
